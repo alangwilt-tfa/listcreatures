97497acb1463e0d397c316b3945dd7363d761bc5
Alan Gwilt <algwilt@gmail.com>
2025-02-02 17:19:12 +0000

    pokemon table migration

---
+++ database/migrations/2025_02_02_152443_create_pokemon_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pokemon', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('dex_number');
+            $table->text('name');
+            $table->text('type_1');
+            $table->text('type_2');
+            $table->integer('hp');
+            $table->integer('attack');
+            $table->integer('defense');
+            $table->integer('speed');
+            $table->integer('special');
+            $table->text('gif');
+            $table->text('png');
+            $table->longText('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pokemon');
+    }
+};
